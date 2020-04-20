991a5213bf0af45e97d001acd8be6fe8c81be131
Ricardo Granja <chirica2501@hotmail.com>
2020-04-20 11:41:38 -0500

    LoginView Components

---
+++ restaurantapp-ios-app/restaurantapp-ios-app/Controller/LoginController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  restaurantapp-ios-app
+//
+//  Created by Ricardo Granja on 20/04/20.
+//  Copyright © 2020 Ricardo Granja. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    @IBOutlet weak var txtUsername: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func btn_SignIn(_ sender: Any) {
+    }
+    
+    @IBAction func btn_SignUp(_ sender: Any) {
+    }
+}
+
